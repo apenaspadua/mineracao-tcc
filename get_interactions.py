cc910ccb96cc9655c9ad5aa2f0a6bb50fed2b060
Matheus de Padua <Padua@Macbook-do-Padua.local>
2020-05-31 14:10:42 -0300

    pegando ids das interacoes

--- get_interactions.py
+++ get_interactions.py
@@ -3,9 +3,18 @@ import pymysql
 from selenium.webdriver import ActionChains
 from random import randint
 import time
+import re
+
 
 # inicializando MySQL
 db = pymysql.connect("localhost", "root", "endgame123", "base_tcc")
+
+
+def formatString(dataItem):
+    data = dataItem[7:-1]
+    replaceData = data.replace(",", "")
+    formatData = replaceData.replace("'", "")
+    return formatData
 
 
 def postToDatabase(id, item):
@@ -26,6 +35,10 @@ def main():
         index = randint(0, 1040)
         if cont <= 2:
             ActionChains(chrome).click(items[index]).perform()
+            data = items[index].get_attribute("onclick")
+            id_item = formatString(data)
+            id = int(re.search(r'\d+', id_item).group(0))
+            print(id)
         else:
             chrome.execute_script('getDados()')
             time.sleep(3)
